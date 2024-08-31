2975751d9eb55d0c0c458c220457783fd6059e07
TheODDYSEY <mosessabz07@gmail.com>
2024-08-31 09:29:37 +0300

    struct & interfaces ts 35:07

--- cmd/tutorial_6/main.go
+++ cmd/tutorial_6/main.go
@@ -6,15 +6,53 @@ type gasEngine struct {
 	// Define the fields of the struct
 	mpg uint8
 	gallons uint8
-	owner
+}
+type electricEngine struct {
+	// Define the fields of the struct
+	kWh uint8
+	mpkwh uint8
 }
 
-type owner struct {
-	name string
+
+
+// method for calculating miles left in tank
+func (e gasEngine) milesLeft() uint8 {
+	// Define a method for the struct
+	return e.mpg * e.gallons
+}
+
+// method for calculating miles left in battery
+func (e electricEngine) milesLeft() uint8 {
+	// Define a method for the struct
+	return e.kWh * e.mpkwh
+}
+
+
+type engine interface{
+	milesLeft() uint8
+}
+
+
+func canMakeIt (e gasEngine , miles uint8){
+	if miles <=e.milesLeft(){
+		fmt.Printf("You can make it with %v miles left\n",e.milesLeft()-miles)
+	}else{
+		fmt.Println("You need to fuel up ")
+	}
 }
 
 func main (){
-	var myEngine gasEngine = gasEngine{25 ,10,owner{"Alex"}}
-	fmt.Println(myEngine.mpg , myEngine.gallons , myEngine.name)
+
+	var myEngine gasEngine = gasEngine{ 30 ,10}
+	canMakeIt(myEngine,200)
+
+	// anonymous struct
+	// var myEngine2 = struct {
+	// 	mpg uint8
+	// 	gallons uint8
+	// }{mpg: 30, gallons: 10}
+
+
+
 
 }
