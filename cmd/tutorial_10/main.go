7a86fc6caa7accaeb6922688a2158a747306b25e
TheODDYSEY <mosessabz07@gmail.com>
2024-09-05 19:05:00 +0300

    func v2 for generics

--- cmd/tutorial_10/main.go
+++ cmd/tutorial_10/main.go
@@ -5,19 +5,14 @@ package main
 import "fmt"
 
 func main(){
-	var intSlice = []int{1,2,3}
-	fmt.Println(sumSlice[int](intSlice))
+	var intSlice = []int{}
+	fmt.Println(isEmpty[int](intSlice))
 
 	var float32Slice = []float32{1,2,3}
-	fmt.Println(sumSlice[float32](float32Slice))
+	fmt.Println(isEmpty[float32](float32Slice))
 
 }
 
-func sumSlice[T int | float32 | float64](slice []T) T{
-	var sum T
-	for _, v:= range slice{
-		sum += v
-	}
-
-	return sum
+func isEmpty[T any](slice []T) bool{
+	return len(slice)==0 
 }
