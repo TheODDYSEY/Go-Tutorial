6f7be68b2f3e6d31bf2094e1c6b62721720535a4
TheODDYSEY <mosessabz07@gmail.com>
2024-09-08 22:15:04 +0300

    building api ts 58:39

--- api-project/api/api.go
+++ api-project/api/api.go
@@ -1,32 +1,30 @@
-package main
+package api
 
 import (
 	"encoding/json"
 	"net/http"
 )
 
-// Coint Balance Params
+// Coin Balance Params
 type CoinBalanceParams struct {
 	Username string
 }
 
-// Coin Balance Response
-type CoinBalanceResponse struct {
-	// Success Code ,Ussually 200
-	Code int 
-
-	// Account Balance
-	Balance int
-}
-
-// ERRor response 
-type ErrorResponse struct {
-
-	// Error Code
+// Coin Balance Response 
+type CoinBalanceResponse struct{
+	// Success Code ,Usually 200
 	Code int
 
-	// Error Message
-	Message string
+
+	// Account Balance 
+	Balance int64
 }
 
+// Error Response 
+type Error struct{
+	// Error code 
+	Code int
 
+	// Error message
+	Message string
+}
